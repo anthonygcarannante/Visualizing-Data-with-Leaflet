e3c4025ab6805163dd1729f57117788ea3683e29
carannantea <79670978+carannantea@users.noreply.github.com>
2021-07-29 22:13:10 -0400

    Added marker radius based on magnitude and color based on depth of earthquake

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,16 +1,33 @@
-
 // Use this link to get the GeoJSON data.
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-// Function for the marker sizes
-function markerSize(magnitude) {
-  var markerOption = {
-    radius: magnitude * 2,
-    color: "red",
-    fillColor: "red",
+// Function for the marker sizes earthquake magnitude. Calls markerColor for depth color.
+function markerStyler(magnitude, depth) {
+  var markerRadius = {
+    radius: magnitude * 4,
+    color: markerColor(depth),
+    fillColor: markerColor(depth),
     fillOpacity: 0.75
+  }  
+  return markerRadius
+}
+
+// Function for marker colors based on depth
+function markerColor(depth) {
+  switch (true) {
+    case depth > 65:
+      return "#cc0000";
+    case depth > 45:
+      return "##e60000";
+    case depth > 25:
+      return "#ff0000";
+    case depth > 10:
+      return "#ff1a1a";
+    case depth > 0:
+      return "#ff3333";
+    default:
+      return "#ff3333";
   }
-  return markerOption
 }
 
 // Grab GeoJSON Data
@@ -22,15 +39,13 @@ d3.json(link).then(function (data) {
 
 function createFeatures(earthquakeData) {
 
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-  
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachfeature: onEachFeature,
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng, markerSize(feature.properties.mag))
+    onEachfeature: function (feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p> ${new Date(feature.properties.time)}</p>`)
+    },
+    pointToLayer: function (feature, latlng) {
+      console.log(feature.geometry.coordinates[2])
+      return L.circleMarker(latlng, markerStyler(feature.properties.mag,feature.geometry.coordinates[2]))
     }
   });
 
@@ -69,10 +84,10 @@ function createMap(earthquakes) {
   // Initialize the Map
   var myMap = L.map("map", {
     center: [38.5816, -121.4944],
-    zoom: 6,
+    zoom: 4,
     layers: [streetmap, earthquakes]
   });
-  
+
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
